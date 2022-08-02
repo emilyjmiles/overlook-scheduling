5a0694434aeee1c59d29087208d900b2b89a40a3
Emily Miles <ms.emily.j.miles@gmail.com>
2022-08-02 17:37:33 -0400

    Add basic structure for test files

--- test/room-test.js
+++ test/room-test.js
@@ -1,9 +1,23 @@
-
 import chai from 'chai';
+import Room from '../src/classes/Room';
+import roomData from '../src/data/roomData';
 const expect = chai.expect;
 
-describe('See if the tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
+describe('Room Class', () => {
+  // Global Variables:
+  let room;
+
+  beforeEach( () => {
+    room = new Room(roomData)
+  })
+
+  it('should be a function', () => {
+ 
+    expect(Room).to.be.a('function');
+  });
+
+  it('should be an istance of Room', () => {
+ 
+    expect(room).to.be.an.instanceOf(Room);
   });
 });
